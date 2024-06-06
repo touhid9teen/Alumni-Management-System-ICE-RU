5036fa6c13468969758d0fc566d8950cc06e470f
Touhidul Islam <toouhidul@gmail.com>
2024-06-06 18:43:44 +0600

    Avatar component is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Toggle from "./elements/Toggle";
+import Avatar from "./components/Avatar";
 const App = () => {
     return (
         <div className="w-80 h-80">
             <h1>This is Alumni Management Project</h1>
-            <Toggle />
+            <Avatar />
         </div>
     );
 };
