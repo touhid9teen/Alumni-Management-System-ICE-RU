16aef9a179ab4bc363015770c694ed5be127a7b7
Touhidul Islam <toouhidul@gmail.com>
2024-07-13 22:14:23 +0600

    Events Page is updated and EventItems component is also updated

--- src/components/Event/EventItem.tsx
+++ src/components/Event/EventItem.tsx
@@ -3,89 +3,67 @@ import Button from "../../elements/Button";
 
 interface EventItemProps {
     //eventId: number;
-    heading: string;
+    title: string;
     date: string;
     startTime: string;
-    endTime: string;
+    endTime?: string;
     location: string;
-    //description: string;
+    description?: string;
     eventImage: string;
 }
 
 const EventItem: React.FC<EventItemProps> = (props: EventItemProps) => {
     const {
         //eventId,
-        heading,
+        title,
         date,
         startTime,
         endTime,
         location,
-        //description,
+        // description,
         eventImage,
     } = props;
 
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center py-5 border-b bg-white shadow-md rounded-lg mb-4">
-            <div className="flex flex-col items-center w-full md:w-1/2">
+        <div className="flex py-5 gap-20 md:gap-10  bg-white shadow-md rounded-lg mb-4">
+            <div className="border w-30 h-30 md:w-1/2">
                 <img
                     src={eventImage}
-                    alt={heading}
-                    className="w-full h-64 object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    alt={title}
+                    className="object-cover w-[50%] rounded-t-lg md:rounded-none md:rounded-l-lg"
                 />
             </div>
 
-            <div className="flex flex-col items-start w-full md:w-1/2 p-5">
+            <div className="flex  flex-col items-start gap-3 md:w-1/2 p-5">
                 <h1 className="text-xl md:text-2xl lg:text-3xl font-bold leading-tight mb-3 ">
-                    {heading}
+                    {title}
                 </h1>
-                <div className="flex flex-col md:flex-row justify-between gap-4 mb-2  text-gray-500">
-                    <div className="flex flex-row justify-start items-center space-x-2">
-                        <p className="text-md md:text-lg lg:text-xl">{date}</p>
-                        <p className="text-md md:text-lg lg:text-xl">
-                            {startTime}
-                        </p>
-                        <span className="text-md md:text-lg lg:text-xl">
-                            to
-                        </span>
-                        <p className="text-md md:text-lg lg:text-xl">
-                            {endTime}
-                        </p>
-                    </div>
-                    <div className="flex flex-row justify-start items-center space-x-2">
-                        <p className="text-md md:text-lg lg:text-xl">
-                            {location}
-                        </p>
-                    </div>
-                </div>
+                <p className="text-md md:text-lg lg:text-xl">
+                    Event Date: {date}
+                </p>
+                <p className="text-md md:text-lg lg:text-xl">
+                    Time: {startTime}
+                </p>
+
+                <p className="text-md md:text-lg lg:text-xl">
+                    Location: {location}
+                </p>
 
                 <div className="flex flex-col md:flex-row justify-end items-center gap-5  w-full">
-                    {/* <button
-                        className="flex justify-center items-center bg-primary text-black px-2 mt-4 w-full md:w-auto h-12 font-semibold text-sm rounded"
-                        type="button"
+                    <Button
+                        buttonType="submit"
+                        customClass="flex justify-center p-3 !py-4 item-center font-semibold !text-textDark bg-tranquil-blue hover:bg-han-purple hover:!text-white"
+                        //disabled={isLoading}
                     >
                         Register Now
-                    </button>
-
-                    <button
-                        className="flex justify-center items-center bg-primary text-black px-2 mt-4 w-full md:w-auto h-12 font-semibold text-sm rounded"
-                        type="button"
+                    </Button>
+                    <Button
+                        buttonType="submit"
+                        customClass="flex justify-center p-3 !py-4 item-center font-semibold !text-textDark bg-tranquil-blue hover:bg-han-purple hover:!text-white"
+                        //disabled={isLoading}
                     >
                         See Details
-                    </button> */}
-                    <Button
-						buttonType="submit"
-						customClass="flex justify-center p-2 item-center font-semibold text-base text-black !py-0"
-						//disabled={isLoading}
-					>
-						Register Now
-					</Button>
-                    <Button
-						buttonType="submit"
-						customClass="flex justify-center p-2 item-center font-semibold text-base text-black !py-0"
-						//disabled={isLoading}
-					>
-						See Details
-					</Button>
+                    </Button>
                 </div>
             </div>
         </div>
--- src/page/Events.tsx
+++ src/page/Events.tsx
@@ -1,113 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EventItem from "../components/Event/EventItem";
 import CreateEventForm from "../components/Event/CreateEventForm";
 import axios from "axios";
+import { LOCAL_STORAGE_KEYS } from "../constants/Global";
+import { getFromStorage } from "../utils/token";
+import { getBaseUrl } from "../hooks/baseUrl";
+import { toast } from "react-toastify";
 
 const Events: React.FC = () => {
     const [createEventForm, showCreateEventForm] = useState<boolean>(false);
-    // const [evventData, setEventData] = useState<>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [eventData, setEventData] = useState<object>({});
     const handleShowEventForm = () => {
         showCreateEventForm(!createEventForm);
-    }
-    const Events = [
-        {
-            eventId: 1,
-            heading: "Stanford GSB United: Annual Gathering 2024",
-            date: "2024-07-04",
-            startTime: "02:30 AM",
-            endTime: "05:30 AM",
-            location: "Good Morning Cafe & Grill Toranomon",
-            eventimage: 'https://picsum.photos/600/400?random=1',
-            description:
-                "Join us for the annual gathering of Stanford GSB United. Connect with alumni and enjoy a delightful morning at the Good Morning Cafe & Grill Toranomon.",
-        },
-        {
-            eventId: 2,
-            heading: "Summer Tango Workshops (Foundations I)",
-            date: "2024-07-04",
-            startTime: "12:45 PM",
-            endTime: "02:00 PM",
-            location: "Oshman Family JCC, Palo Alto",
-            eventimage: 'https://picsum.photos/600/400?random=2',
-            description:
-                "Kickstart your summer with our Tango Foundations I workshop. Perfect for beginners looking to learn the basics of Tango dancing.",
-        },
-        {
-            eventId: 3,
-            heading: "Summer Tango Workshops (Foundations II)",
-            date: "2024-07-04",
-            startTime: "02:45 PM",
-            endTime: "04:00 PM",
-            location: "Feeling Flow Studio, Palo Alto",
-            eventimage: 'https://picsum.photos/600/400?random=3',
-            description:
-                "Continue your Tango journey with our Foundations II workshop. Designed for those who have completed the Foundations I workshop or have some Tango experience.",
-        },
-        {
-            eventId: 4,
-            heading: "Tech Innovators Meetup 2024",
-            date: "2024-08-15",
-            startTime: "10:00 AM",
-            endTime: "12:00 PM",
-            location: "Silicon Valley Conference Center",
-            eventimage: 'https://picsum.photos/600/400?random=4',
-            description:
-                "Network with tech innovators and enthusiasts at our annual meetup. Discover the latest trends and share ideas with industry leaders.",
-        },
-        {
-            eventId: 5,
-            heading: "Annual Charity Run",
-            date: "2024-09-10",
-            startTime: "06:00 AM",
-            endTime: "09:00 AM",
-            location: "Central Park, New York",
-            eventimage: 'https://picsum.photos/600/400?random=5',
-            description:
-                "Participate in our annual charity run to support local communities. Enjoy a morning run through Central Park and make a difference.",
-        },
-        {
-            eventId: 6,
-            heading: "Digital Marketing Workshop",
-            date: "2024-10-05",
-            startTime: "09:00 AM",
-            endTime: "05:00 PM",
-            location: "Online Event",
-            eventimage: 'https://picsum.photos/600/400?random=6',
-            description:
-                "Join our comprehensive digital marketing workshop to enhance your skills. Learn from experts and gain practical knowledge to boost your career.",
-        },
-        {
-            eventId: 7,
-            heading: "Art & Wine Festival",
-            date: "2024-11-20",
-            startTime: "01:00 PM",
-            endTime: "06:00 PM",
-            location: "Downtown Plaza, San Francisco",
-            eventimage: 'https://picsum.photos/600/400?random=31',
-            description:
-                "Enjoy a day of art exhibitions and wine tasting at our annual Art & Wine Festival. Meet local artists and sample fine wines from the region.",
-        },
-        {
-            eventId: 8,
-            heading: "Leadership Summit 2024",
-            date: "2024-12-10",
-            startTime: "08:00 AM",
-            endTime: "05:00 PM",
-            location: "Hilton Hotel, Los Angeles",
-            eventimage: 'https://picsum.photos/600/400?random=32',
-            description:
-                "Attend our Leadership Summit to learn from top industry leaders. Gain insights into effective leadership strategies and network with professionals.",
-        },
-    ];
-    // const fetchData = async (params:type) => {
-    //     try {
-    //         const response = await axios.get('https://your-backend-api.com/data');
-    //         console.log(response.data); // The fetched data
-    //       } catch (error) {
-    //         console.error('There was an error fetching the data:', error);
-    //       }
-    // }
-    // fetchData();
+    };
+
+    const token = getFromStorage(LOCAL_STORAGE_KEYS.AUTH_TOKEN);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const url = getBaseUrl() + "/events";
+                const response = await axios.get(url, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "multipart/form-data",
+                    },
+                });
+                setEventData(response?.data?.Data);
+                toast.success("Event create successful!", {
+                    autoClose: 1500,
+                });
+            } catch (error) {
+                const errorMessage =
+                    typeof error?.response?.data === "string"
+                        ? error.response.data
+                        : "An unexpected error occurred. Please try again.";
+                toast.error(errorMessage, {
+                    autoClose: 3000,
+                });
+            }
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
     return (
         <div className="flex flex-col font-serif">
             <div className="flex flex-col justify-center items-center pt-10 pb-16 sm:pt-20">
@@ -117,24 +54,28 @@ const Events: React.FC = () => {
                 <p className="text-xl sm:text-2xl pt-4">
                     Peek at some alumni events happening just around the corner.
                 </p>
-                <button className="text-xl sm:text-2xl pt-4 cursor-pointer text-blue-500 underline" 
+                <button
+                    className="text-xl sm:text-2xl pt-4 cursor-pointer text-blue-500 underline"
                     onClick={handleShowEventForm}
                 >
                     Write a Event ?
                 </button>
-                { createEventForm &&   <CreateEventForm />}
+                {createEventForm && <CreateEventForm />}
             </div>
-            {Events.map((event) => (
-                <EventItem
-                    key={event.eventId}
-                    heading={event.heading}
-                    date={event.date}
-                    startTime={event.startTime}
-                    endTime={event.endTime}
-                    eventImage={event.eventimage}
-                    location={event.location}
-                />
-            ))}
+            {eventData.length > 0 ? (
+                eventData.map((event) => (
+                    <EventItem
+                        title={event.title}
+                        date={event.event_date}
+                        startTime={event.start_time}
+                        location={event.location}
+                        description={event.description}
+                        eventImage={event.image_path}
+                    />
+                ))
+            ) : (
+                <div>No events found.</div> // Optional: Display when there are no events
+            )}
         </div>
     );
 };
